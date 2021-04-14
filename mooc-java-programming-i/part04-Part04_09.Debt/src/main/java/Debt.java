03e45488417da400a133853697015e8d2816b11b
Dennis-V1 <71431080+Dennis-V1@users.noreply.github.com>
2021-04-14 08:26:59 +0200

    rework of some tasks and work until task 15

---
+++ mooc-java-programming-i/part04-Part04_09.Debt/src/main/java/Debt.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dennis
+ */
+public class Debt {
+    double balance;
+    double interestRate;
+    public Debt(double initialBalance, double initialInterestRate){
+        
+    }
+    public void printBalance(){
+        System.out.println(balance);
+    }
+    public void waitOneYear(){
+        balance = balance * interestRate;
+    }
+}
