ab239b6ec4a9bd4b3a3ceb17497c0e8c6d1f5a5d
Dennis-V1 <71431080+Dennis-V1@users.noreply.github.com>
2021-05-05 15:04:31 +0200

    überarbeitung von ein paar dingen

--- mooc-java-programming-i/mavenproject1/src/main/java/Abfrage.java
+++ mooc-java-programming-i/mavenproject1/src/main/java/Abfrage.java
@@ -6,94 +6,71 @@ import java.io.FileNotFoundException;
 
 public class Abfrage {
     Scanner abfragen = new Scanner(System.in);
-    Scanner productbuy = new Scanner(System.in);
-    Scanner mengenabfrage = new Scanner(System.in);
     String begruessung;
     String zuKaufenderArtikel;
     int productid;
     int menge;
-    File arrayListFile = new File("C:\\Users\\Dennis\\Documents\\NetBeansProjects\\mavenproject1\\src\\main\\java\\data.txt");
-    ArrayList<Artikel> listeArtikel = new ArrayList<>();
-    
-    public ArrayList<Artikel> getarraylist() throws FileNotFoundException{
-        double preis;
-        int menge1;
-        String bez;
-        int id;
-              
-        String [] parts;
-           
-        Scanner scanner = new Scanner(arrayListFile);
-           
-        while (scanner.hasNextLine()){
-            String line = scanner.nextLine();
-               
-            parts = line.split(",");
-               
-            id = Integer.parseInt(parts[0]);
-            bez = parts[1];
-            preis = Double.valueOf(parts[2]);
-            menge1 = Integer.parseInt(parts[3]);
-               
-            Artikel art = new Artikel(id , bez , preis , menge1);
-            listeArtikel.add(art);
-        }
-           return listeArtikel;
-    }
-       
-                
+    File artikelFile = new File("C:\\Users\\Dennis\\Documents\\NetBeansProjects\\mavenproject1\\src\\main\\java\\Artikel.txt");
+    ArrayList<Artikel> artikelListe = new ArrayList<>();
+    double preis;
+    int menge1;
+    String bez;
+    int id;
+    String [] parts;
 
+       
+    public void readArticleFromFile(String fileLine) throws FileNotFoundException{   
+        Scanner scanner = new Scanner(artikelFile);
+        while (scanner.hasNextLine()){
+            String line =scanner.nextLine();
+            Artikel art = getArticleFromLine(line);
+            artikelListe.add(art);
+        }
+    }
+        
+    public Artikel getArticleFromLine(String fileLine){  
+        parts = fileLine.split(",");               
+        id = Integer.parseInt(parts[0]);
+        bez = parts[1];
+        preis = Double.valueOf(parts[2]);
+        menge1 = Integer.parseInt(parts[3]);
+               
+        Artikel art = new Artikel(id , bez , preis , menge1);
+        return art;
+    }
+     
     public void Gruss(String begruessung) {
         begruessung = "Guten Tag lieber Kunde was möchten sie gerne Kaufen?";
         System.out.println(begruessung);
     }
     public void Kaufen(String zuKaufenderArtikel) {
         
-        rep:{
+        //rep:{
+        while(true){
         System.out.println("wollen sie einen Artikel kaufen?");
         String antwortKundeaufFrage = abfragen.next();
         if ("y".equals(antwortKundeaufFrage)) {
             System.out.println("Welches Produkt möchten sie gerne kaufen?");
-            productid = productbuy.nextInt();
+            productid = abfragen.nextInt();
             //ich muss die ids in einem extra array speicher um dieses am ende aus zu lesen
-            for (Artikel art : listeArtikel ){
+            for (Artikel art : artikelListe ){
                 // int id = art.getId();  nichtmehr benötigt :)
                 String bez = art.getArtikelBez();
                 int anzahlen = 0;
                 System.out.println("Wie viele" + bez + "wollen sie kaufen?"); anzahlen = abfragen.nextInt(); 
                 art.setanzahlKaufen(anzahlen);
                 }
-            /* if("10".equals(productid)){
-                System.out.println("Wie viele" + "" + "wollen sie kaufen?");
-                menge = mengenabfrage.nextInt();
-            }
-            if("11".equals(productid)){
-                 
-            } 
-            if("12".equals(productid)){
-                
-            }
-            if("13".equals(productid)){
-                
-            }
-            if("14".equals(productid)){
-                
-            }
-            if("15".equals(productid)){
-                
-            }
-            if("16".equals(productid)){
-                
-            } */
 
-        } else if("n".equals(antwortKundeaufFrage)){
+            } else if("n".equals(antwortKundeaufFrage)){
             System.out.println("Danke für ihren Besuch, das Programm wird automatisch geschlossen!");
-            System.exit(0);
-        } else {
+            break;
+            } else {
             //ich muss hier noch eine falscheingabe implementieren
-            System.out.println("Ihre eingabe war inkorrekt, bitte geben sie y für Ja und n für Nein ein.");
-            break rep;   //https://stackoverflow.com/questions/18159965/errorundefined-label-how-to-use-label-statement-in-this-code-in-java
+                System.out.println("Ihre eingabe war inkorrekt, bitte geben sie y für Ja und n für Nein ein.");
+                //break rep;   //https://stackoverflow.com/questions/18159965/errorundefined-label-how-to-use-label-statement-in-this-code-in-java
             }
         }
+        // ende while
+        
     }
 }
--- mooc-java-programming-i/mavenproject1/src/main/java/shopliste.java
+++ mooc-java-programming-i/mavenproject1/src/main/java/shopliste.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 public class shopliste {
     
     public static void main(String[] args) {
-        ArrayList<Artikel> listeArtikel = new ArrayList<>();
-        Artikel newSilvanTurbo = new Artikel(10, "New Silvan Turbo", 7.5, 20);
-	Artikel afrimundBashkim = new Artikel(11, "Afrim und Bashkim", 6, 20);
-	Artikel danielalleinzuHaus = new Artikel(12, "Daniel allein zu Haus", 4.5, 20);
-	Artikel callofDutyPhillipsWarPC = new Artikel(13, "Call of Duty - Phillips War PC", 30, 20);
-	Artikel callofDutyPhillipsWarPS4 = new Artikel(14, "Call of Duty - Phillips War PS4", 40, 20);
-	Artikel cybernico2077PC = new Artikel(15, "Cybernico 2077 PC", 40, 20);
-	Artikel cybernico2077PS4 = new Artikel(16, "Cybernico 2077 PS4", 50, 20);
+        //ArrayList<Artikel> listeArtikel = new ArrayList<>();
+        //Artikel newSilvanTurbo = new Artikel(10, "New Silvan Turbo", 7.5, 20);
+	//Artikel afrimundBashkim = new Artikel(11, "Afrim und Bashkim", 6, 20);
+	//Artikel danielalleinzuHaus = new Artikel(12, "Daniel allein zu Haus", 4.5, 20);
+	//Artikel callofDutyPhillipsWarPC = new Artikel(13, "Call of Duty - Phillips War PC", 30, 20);
+	//Artikel callofDutyPhillipsWarPS4 = new Artikel(14, "Call of Duty - Phillips War PS4", 40, 20);
+	//Artikel cybernico2077PC = new Artikel(15, "Cybernico 2077 PC", 40, 20);
+	//Artikel cybernico2077PS4 = new Artikel(16, "Cybernico 2077 PS4", 50, 20);
         
         
         Abfrage1();
